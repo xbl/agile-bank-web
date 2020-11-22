8e529b131bf5bdd01eb6f5c53c82fdbe3e779fd1
Jing-lucky <lyj280664576@gmail.com>
2020-11-22 20:02:47 +0800

    feat (Question): 验证问题是否有答案，添加交卷按钮

--- src/modules/Question.vue
+++ src/modules/Question.vue
@@ -1,25 +1,28 @@
 <template>
-  <div class="question-wrap">
+  <div class="question-wrap" v-if="qesShow">
     <div class="question-body">
       <SingleChoice v-if="qesCurrent.type === 'SingleSelection'"
         :id="qesCurrent.id"
         :title="qesCurrent.title"
         :hint="qesCurrent.hint"
         :options="qesCurrent.options"
-        v-model="SingleValue">
+        v-model="singleValue">
       </SingleChoice>
       <MultipleChoice v-else-if="qesCurrent.type === 'MultiSelection'"
         :id="qesCurrent.id"
         :title="qesCurrent.title"
         :hint="qesCurrent.hint"
         :options="qesCurrent.options"
-        v-model="MultipleValue">
+        v-model="multipleValue">
       </MultipleChoice>
       <div v-else>{{ qesCurrent.id}}:{{qesCurrent.title }}</div>
     </div>
     <div class="question-button">
       <button type="button" class="qusbtn" @click="preQesFn" :disabled="preQesBtn">上一题</button>
       <button type="button" class="qusbtn" @click="nextQesFn" :disabled="nextQesBtn">下一题</button>
+    </div>
+    <div class="question-submit" v-if="numCurrent === (qesList.length - 1)">
+      <input type="submit" class="subbtn" value="交卷" @click="submitQesFn">
     </div>
   </div>
 </template>
@@ -37,11 +40,19 @@ import API from '@/api';
   },
 })
 export default class Question extends Vue {
+  qesShow = false;
+
   qesList = [];
 
-  SingleValue = '';
+  answerList = new Map();
 
-  MultipleValue = [];
+  singleValueList: Array<string> = new Array<string>();
+
+  singleValue = '';
+
+  multipleValueList: Array<Array<string>> = new Array<Array<string>>();
+
+  multipleValue = [];
 
   qesCurrent = {};
 
@@ -49,7 +60,7 @@ export default class Question extends Vue {
 
   preQesBtn =true;
 
-  nextQesBtn = false;
+  nextQesBtn = true;
 
   @Watch('numCurrent')
   numCurrentChange() {
@@ -66,6 +77,30 @@ export default class Question extends Vue {
     }
   }
 
+  @Watch('singleValue')
+  singleValueChange() {
+    console.log('singleValueChange');
+    if (this.singleValue !== '') {
+      this.nextQesBtn = false;
+      this.singleValueList[this.numCurrent] = this.singleValue;
+    } else {
+      this.nextQesBtn = true;
+    }
+    console.log(this.singleValueList);
+  }
+
+  @Watch('multipleValue')
+  multipleValueChange() {
+    console.log('multipleValueChange');
+    if (this.multipleValue.length !== 0) {
+      this.nextQesBtn = false;
+      this.multipleValueList[this.numCurrent] = this.multipleValue;
+    } else {
+      this.nextQesBtn = true;
+    }
+    console.log(this.singleValueList);
+  }
+
   preQesFn() {
     if (this.numCurrent > 0) {
       this.numCurrent -= 1;
@@ -78,6 +113,14 @@ export default class Question extends Vue {
       this.numCurrent += 1;
     }
     this.qesCurrent = this.qesList[this.numCurrent];
+    this.singleValue = '';
+    this.multipleValue = [];
+  }
+
+  submitQesFn() {
+    console.log('交卷');
+    console.log(this.singleValueList);
+    console.log(this.multipleValueList);
   }
 
   public async mounted() {
@@ -85,6 +128,7 @@ export default class Question extends Vue {
     console.log(res);
     this.qesList = res.data;
     [this.qesCurrent] = this.qesList;
+    this.qesShow = true;
   }
 }
 </script>
@@ -92,21 +136,33 @@ export default class Question extends Vue {
 <style lang="stylus" scoped>
 .question-wrap
   margin 0 auto
-  width 30%
+  width 400px
   .question-body
     height 45vh
   .question-button
     display flex
     .qusbtn
-      width: 100px;
-      height: 30px;
-      flex: 1;
-      margin: auto 50px;
-      border: 1px solid;
-      border-radius: 15px;
+      width 100px
+      height 30px
+      flex 1
+      margin auto 50px
+      border 1px solid
+      border-radius 15px
       &:hover
         cursor pointer
     .qusbtn[disabled]
       &:hover
         cursor not-allowed
+  .question-submit
+    margin-top 20px
+    .subbtn
+      width 330px
+      height 33px
+      color #ffffff
+      border unset
+      border-radius 10px
+      background-color #e9686b
+      letter-spacing 20px
+      &:hover
+          background-color #ba3537
 </style>
