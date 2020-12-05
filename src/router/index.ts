ac1f484176a274ca19e25ea908e07d292302ee1e
xbl <x_bl@sina.com>
2020-12-05 15:28:14 +0800

    refactor(router): add finished router

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,12 +12,9 @@ const routes: Array<RouteConfig> = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../modules/About.vue'),
+    path: '/finished',
+    name: 'finished',
+    component: () => import(/* webpackChunkName: "about" */ '../modules/finished.vue'),
   },
   {
     path: '/',
