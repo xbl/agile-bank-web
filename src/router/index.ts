5ac1badc04befda4150b058af0641f2c231cc622
xbl <x_bl@sina.com>
2020-12-05 16:48:26 +0800

    refactor(Index): 删除没用的页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,10 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '@/modules/Home.vue';
 import Question from '@/modules/Question.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/home',
-    name: 'Home',
-    component: Home,
-  },
   {
     path: '/finished',
     name: 'finished',
